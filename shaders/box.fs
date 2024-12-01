0b7cdd20eeb60027a45ee542c40dbc93f3d3bd5a
Hob3rt <1215315078@qq.com>
2024-12-01 11:26:36 +0800

    投光物

--- shaders/box.fs
+++ shaders/box.fs
@@ -7,11 +7,18 @@ struct Material {
     float shininess;
 }; 
 struct Light {
-    vec3 position;
+    vec3 position; // 点光源
+    vec3 direction;//定向光 一起出现时 聚光
+    float cutOff;
+    float outerCutOff;
 
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
 };
 
 in vec3 Normal;
@@ -24,10 +31,18 @@ uniform vec3 viewPos;
 
 void main()
 {
+    float distance    = length(light.position - FragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + 
+                light.quadratic * (distance * distance));
+    vec3 norm = normalize(Normal);
+    //vec3 lightDir = normalize(-light.direction);//定向光
+    vec3 lightDir = normalize(light.position - FragPos);//点光源
+    float theta = dot(lightDir, normalize(-light.direction));
+    float epsilon   = light.cutOff - light.outerCutOff;
+    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+    
     vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
 
-    vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;
 
@@ -36,6 +51,16 @@ void main()
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
     vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;
 
+    ambient  *= attenuation; 
+    diffuse  *= attenuation;
+    specular *= attenuation;
+
+    diffuse  *= intensity;
+    specular *= intensity;
+
     vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
+
+
+    
 }
